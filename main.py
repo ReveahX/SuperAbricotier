1ea36a686bafc69601cb3b21db3bcb24ae47f1d9
ReveahX <rayhan.lah@hotmail.com>
2022-12-09 15:00:04 +0100

    new file: Player.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Drexa")
+screen = pygame.display.set_mode((1080, 720))
+
+
+#background = pygame.image.load('assets/bg.png')
+
+
+Running = True
+
+while Running:
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            Running = False
+            pygame.quit()
+
+print("Tu pars déja")
