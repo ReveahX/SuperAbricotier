5a7ae9801fa616d11ac68d9acf78a09827892a1d
ReveahX <rayhan.lah@hotmail.com>
2022-12-09 15:10:10 +0100

    new file: Game.py     new file: Player.py

---
+++ Game.py
@@ -0,0 +1,11 @@
+import pygame
+from Player import Player
+
+class Game:
+
+    def __init__(self):
+        #Obligé de crée un groupe de joueur meme en solo 😭
+        self.all_player = pygame.sprite.Group()
+        self.player = Player(self)
+        self.all_player.add(self.player)
+        
